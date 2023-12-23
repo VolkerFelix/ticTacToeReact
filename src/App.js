949c291f575e4bcd0003dec60b4e57d521856334
VolkerFelix <volker.leukhardt@gmail.com>
2023-12-23 13:36:26 +0100

    next: implement token mint for winners batch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import { useState } from 'react';
+import { Keypair, Connection, PublicKey, Cluster, clusterApiUrl,
+  LAMPORTS_PER_SOL, Transaction, SystemProgram, sendAndConfirmTransaction
+} from "@solana/web3.js";
+
+import secret_x_json from "./keypair/player_x.json";
+import secret_o_json from "./keypair/player_o.json";
+
+const secret_x_a_u8 = Uint8Array.from(secret_x_json);
+const secret_o_a_u8 = Uint8Array.from(secret_o_json);
+const kp_x = Keypair.fromSecretKey(secret_x_a_u8);
+const kp_o = Keypair.fromSecretKey(secret_o_a_u8);
+
+class CPlayer {
+  constructor(name, pk) {
+    this.m_name = name;
+    this.m_pk = pk;
+  }
+}
+
+const player_x = new CPlayer('X', kp_x.publicKey.toString());
+const player_o = new CPlayer('O', kp_o.publicKey.toString());
+let players = [];
+players.push(player_x);
+players.push(player_o);
+
+function PlayerInfo() {
+  let players_render = []
+  for (let i = 0; i < players.length; i++) {
+    players_render.push(
+      <div className="player-info">
+        <p>Name: {players[i].m_name}</p>
+        <p>PublicKey: {players[i].m_pk}</p>
+    </div>
+    )
+  }
+  return players_render;
+}
 
 function Square({value, onSquareClick, isWinner}) {
   let cssClass = "square"
   if (isWinner) {
-    cssClass = "winner_square"
+    cssClass = "winner-square"
   }
   return (
     <button
@@ -15,8 +52,10 @@ function Square({value, onSquareClick, isWinner}) {
 }
 
 function Board({xIsNext, squares, onPlay}) {
+  const winner = calcWinner(squares);
+
   function handleClick(i){
-    if (squares[i] || calcWinner(squares)) {
+    if (squares[i] || winner) {
       return;
     }
     const nextSquares = squares.slice();
@@ -29,16 +68,30 @@ function Board({xIsNext, squares, onPlay}) {
     onPlay(nextSquares);
   }
 
-  const winner = calcWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  }
-  else {
-    status = "Next Player: " + (xIsNext ? "X" : "O");
+  function GameStatus() {
+    let status = [];
+    if (winner) {
+      status.push(
+        <div className="game-status">
+          <div>
+            <p>Winner: {winner[0]}</p>
+          </div>
+          <div>
+            <button onClick={() => claimWinnersBatch()}>Claim batch</button>
+          </div>
+        </div>
+      );
+    }
+    else {
+      status.push(
+        <div className="game-status">
+          <p>Next Player: {(xIsNext ? "X" : "O")}</p>
+        </div>);
+    }
+    return status;
   }
 
-  function RenderBoard(){
+  function RenderBoard() {
     let board = [];
     let row = [];
     let row_idx = 0;
@@ -65,7 +118,10 @@ function Board({xIsNext, squares, onPlay}) {
   }
   
   return (
-    <RenderBoard /> 
+    <>
+      <RenderBoard /> 
+      <GameStatus />
+    </>
   );
 }
 
@@ -102,14 +158,19 @@ export default function Game() {
   )
 
   return(
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+    <>
+      <div>
+        <PlayerInfo />
       </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -131,4 +192,8 @@ function calcWinner(squares) {
     }
   }
   return null;
+}
+
+function claimWinnersBatch(){
+  /* TODO */
 }
